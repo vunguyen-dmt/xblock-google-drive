25639e06a3b281541a1d424863d18d8e36fa8757
Martyn James <mjames@edx.org>
2014-12-05 16:53:27 -0500

    Test Me without setup.py changes

--- setup.py
+++ setup.py
@@ -29,9 +29,7 @@ setup(
     ],
     install_requires=[
         'XBlock',
-        'xblock-utils',
     ],
-    dependency_links = ['http://github.com/edx-solutions/xblock-utils/tarball/master#egg=xblock-utils'],
     entry_points={
         'xblock.v1': [
             'google-document = google_drive:GoogleDocumentBlock',
